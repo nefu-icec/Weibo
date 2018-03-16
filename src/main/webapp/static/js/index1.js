97215129d24473e892237711b0cafab9887f1fc8
贺妍 <490817982@qq.com>
2018-03-16 12:40:47 +0800

    modified index.html add css

--- src/main/webapp/static/js/index1.js
+++ src/main/webapp/static/js/index1.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     WeiboManager.search("2010-10-10", "2010-10-11", 0, 0,function (data) {
-        console.log(data.length);
         new Vue({
             el: "#sum",
             data: {
