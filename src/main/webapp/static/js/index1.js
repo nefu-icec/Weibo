a7d0bc4bc29ff1a4d37305accf32adaac7e01ed4
贺妍 <490817982@qq.com>
2018-03-17 21:20:53 +0800

    modified some main page bug

--- src/main/webapp/static/js/index1.js
+++ src/main/webapp/static/js/index1.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
+
     WeiboManager.search("2010-10-10", "2010-10-11", 0, 0,function (data) {
+        console.log(data.data);
         new Vue({
             el: "#sum",
             data: {
-                sum: data.length
+                sum: data.data.length
             }
         });
         new Vue({
             el: "#shows",
             data: {
-                shows: data
+                shows: data.data
             }
         });
     });
